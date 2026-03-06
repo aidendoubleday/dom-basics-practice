8378ae353ccaee19404b3c0d984312f5d034f030
aidendoubleday <1301279068@s.tusd1.org>
2026-03-06 15:41:48 +0000

    progress

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("Script started");
+
+function changeColor() {
+    console.log("click");
+
+    let p = document.getElementById("poem");
+    let color = prompt("Enter a new color for the poem");
+    p.style.color = color;
+}
+
+function changeFontSize() {
+    console.log("click");
+
+    let p = document.getElementById("poem");
+    let fontSize = prompt("Enter a new font size in pixels");
+    p.style.fontSize = fontSize;
+}
+
+function changePoem() {
+    console.log("click");
+
+    let h1 = document.getElementById("title");
+    let 
+}
